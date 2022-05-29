8fd2ba2549cdebc5ec0f33148bfda38aae907684
devaw-de <it@alex-will.de>
2022-05-29 20:31:54 +0200

    added spell book component

---
+++ src/app/components/modals/spell-book/spell-book.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-spell-book',
+  templateUrl: './spell-book.component.html',
+  styleUrls: []
+})
+export class SpellBookComponent {
+
+}
