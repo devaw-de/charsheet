70e7d1cb5b304051055a1cf01a63e47c17e3ec68
devaw-de <it@alex-will.de>
2022-05-31 19:48:57 +0200

    cleaned up EditCurrencyComponent

--- src/app/components/modals/edit-currency/edit-currency.component.ts
+++ src/app/components/modals/edit-currency/edit-currency.component.ts
@@ -9,28 +9,28 @@ import {Currency} from 'src/library/models/src/lib/src/equipment';
 })
 export class EditCurrencyComponent implements AfterViewInit {
 
-  @ViewChild('platinum', {static: true}) platinum: ElementRef;
-  @ViewChild('gold', {static: true}) gold: ElementRef;
-  @ViewChild('silver', {static: true}) silver: ElementRef;
-  @ViewChild('copper', {static: true}) copper: ElementRef;
+  @ViewChild('platinum', {static: true}) platinum: ElementRef<HTMLInputElement>;
+  @ViewChild('gold', {static: true}) gold: ElementRef<HTMLInputElement>;
+  @ViewChild('silver', {static: true}) silver: ElementRef<HTMLInputElement>;
+  @ViewChild('copper', {static: true}) copper: ElementRef<HTMLInputElement>;
 
   constructor(
     private _dialogRef: DialogRef
   ) { }
 
   ngAfterViewInit(): void {
-    this.platinum.nativeElement.value = this._dialogRef.data.cash.platinum ? this._dialogRef.data.cash.platinum  : 0;
-    this.gold.nativeElement.value = this._dialogRef.data.cash.gold ? this._dialogRef.data.cash.gold : 0;
-    this.silver.nativeElement.value = this._dialogRef.data.cash.silver ? this._dialogRef.data.cash.silver : 0;
-    this.copper.nativeElement.value = this._dialogRef.data.cash.copper ? this._dialogRef.data.cash.copper : 0;
+    this.platinum.nativeElement.value = this._dialogRef.data.cash.platinum ?? 0;
+    this.gold.nativeElement.value = this._dialogRef.data.cash.gold ??  0;
+    this.silver.nativeElement.value = this._dialogRef.data.cash.silver ?? 0;
+    this.copper.nativeElement.value = this._dialogRef.data.cash.copper ?? 0;
   }
 
   public save(): void {
     this._dialogRef.close({
-      platinum: this.platinum.nativeElement.value,
-      gold: this.gold.nativeElement.value,
-      silver: this.silver.nativeElement.value,
-      copper: this.copper.nativeElement.value
+      platinum: parseInt(this.platinum.nativeElement.value, 10),
+      gold: parseInt(this.gold.nativeElement.value, 10),
+      silver: parseInt(this.silver.nativeElement.value, 10),
+      copper: parseInt(this.copper.nativeElement.value, 10)
     } as Currency);
   }
 
