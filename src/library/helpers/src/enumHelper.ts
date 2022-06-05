09200f367f8bf51c211718216211cf42fafa1e30
devaw-de <it@alex-will.de>
2022-06-05 16:36:29 +0200

    made enumToArray public

--- src/library/helpers/src/enumHelper.ts
+++ src/library/helpers/src/enumHelper.ts
@@ -2,38 +2,38 @@ import {Alignment, Attribute, CharacterBackground, CharacterClassName, Character
 
 export class EnumHelper {
 
-  private static _enumToArray(e: any): Array<any> {
+  public static enumToArray(e: any): Array<any> {
     const arr = [];
     Object.values(e).forEach(item => arr.push(item));
     return arr;
   }
 
   public static getAlignmentsList(): Array<Alignment> {
-    return EnumHelper._enumToArray(Alignment);
+    return EnumHelper.enumToArray(Alignment);
   }
 
   public static getAttributesList(): Array<Attribute> {
-    return EnumHelper._enumToArray(Attribute);
+    return EnumHelper.enumToArray(Attribute);
   }
 
   public static getBackgroundList(): Array<CharacterBackground> {
-    return EnumHelper._enumToArray(CharacterBackground);
+    return EnumHelper.enumToArray(CharacterBackground);
   }
 
   public static getClassesList(): Array<CharacterClassName> {
-    return EnumHelper._enumToArray(CharacterClassName);
+    return EnumHelper.enumToArray(CharacterClassName);
   }
 
   public static getLanguagesList(): Array<Language> {
-    return EnumHelper._enumToArray(Language);
+    return EnumHelper.enumToArray(Language);
   }
 
   public static getRacesList(): Array<CharacterRace> {
-    return EnumHelper._enumToArray(CharacterRace);
+    return EnumHelper.enumToArray(CharacterRace);
   }
 
   public static getSkillList(): Array<SkillName> {
-    return EnumHelper._enumToArray(SkillName);
+    return EnumHelper.enumToArray(SkillName);
   }
 
 }
